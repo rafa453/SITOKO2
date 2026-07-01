ee1b24d34a494c3935bcff2b4b23b7c27b814a6c
rafa453 <191682646+rafa453@users.noreply.github.com>
2026-07-01 12:47:03 +0700

    tambah import

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Models\ActivityLog;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
